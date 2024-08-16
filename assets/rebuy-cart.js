6fd74a66d8f6aef4c0653698f92bdc4800b14f38
xtianbong <christianmbong45@gmail.com>
2024-08-16 13:15:46 -0400

    integrated the removal of the lower gift section into the rebuy-cart.js file.

--- assets/rebuy-cart.js
+++ assets/rebuy-cart.js
@@ -1,14 +1,19 @@
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
 
+//modifications to the smart cart widget on render
 const callback = function(mutationsList, observer) {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
             const element = document.getElementsByClassName('rebuy-cart__progress-step-label')[1];
             if (element) {
-                console.log("This lol");
                 console.log(element)
+                //if the progress step label loads on the page, change it's contents to a generic "Free T-Shirt"
                 element.innerHTML = "Free T-Shirt";
+                //remove the specific "Free Gift" from the bottom of the smart cart widget
+                document.querySelectorAll('.rebuy-cart__progress-bar-container.below').forEach(function(element) {
+                    element.style.display = 'none';
+                });
                 observer.disconnect();
             }
         }
@@ -20,8 +25,10 @@ const observer = new MutationObserver(callback);
 // Initial check
 const initialElement = document.querySelector('.rebuy-cart__progress-step-label');
 if (initialElement) {
-    console.log("This lol");
     initialElement.innerHTML = "Free T-Shirt";
 } else {
     observer.observe(targetNode, config);
 }
+
+
+
