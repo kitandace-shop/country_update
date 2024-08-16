8dfebcdd3a4ab45cd0eeb7d90737a94dce188ee4
xtianbong <christianmbong45@gmail.com>
2024-08-16 13:21:14 -0400

    made add to cart unclickable

--- assets/rebuy-cart.js
+++ assets/rebuy-cart.js
@@ -1,7 +1,12 @@
+//make the add to bag button unclickable until the rebuy widget loads
+document.querySelector("#productStickyContainer").style.pointerEvents = 'none';
+
+
+
 const targetNode = document.body;
 const config = { childList: true, subtree: true };
 
-//modifications to the smart cart widget on render
+//modifications to the smart cart widget on render use mutation observer to detect the loading of the object
 const callback = function(mutationsList, observer) {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
